8390d84c2a13ec8dbed2a57ec03bbee6d052993d
michael <Michael.Golden0278@gmail.com>
2025-02-03 23:32:00 -0500

    obsidian: format

--- modules/programs/obsidian.nix
+++ modules/programs/obsidian.nix
@@ -9,13 +9,12 @@ let
       enable = mkEnableOption "plugin";
       config = mkOption {
         type = types.attrs;
-        default = {};
+        default = { };
         description = "Plugin configuration";
       };
     };
   };
-in 
-{
+in {
   meta.maintainers = with lib.hm.maintainers; [ michaelgoldenn ];
 
   options.programs.obsidian = {
@@ -33,7 +32,7 @@ in
           };
           community-plugins = mkOption {
             type = types.attrsOf pluginConfigType;
-            default = {};
+            default = { };
             description = ''
               Attribute set of community plugins.
               Each key is the plugin name and the value is its configuration.
@@ -56,7 +55,7 @@ in
           };
           core-plugins = mkOption {
             type = types.attrsOf pluginConfigType;
-            default = {};
+            default = { };
             description = ''
               Attribute set of core plugins.
               Each key is the plugin name and the value is its configuration.
@@ -79,67 +78,78 @@ in
           };
           extraConfig = mkOption {
             type = lib.types.attrsOf (lib.types.attrs);
-            default = {};
-            description = "
-            Additional configuration for obsidian vault.
-            ";
-            example = "
-            app = { /* configuration to place into vault/.obsidian/app */}";
+            default = { };
+            description =
+              "Additional configuration for obsidian vault.";
+            example =
+              "app = { /* configuration to place into vault/.obsidian/app */}";
           };
         };
       });
-      default = {};
+      default = { };
       description = "Attribute set of obsidian vaults";
     };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
-    
+
     home.file = let
-      vaultFiles = mapAttrsToList (vaultName: vaultCfg: let
-        enabled = vaultCfg.enable;
-        enabledCorePlugins = filterAttrs (_: p: p.enable) vaultCfg.core-plugins;
-        corePluginsList = attrNames enabledCorePlugins;
-        enabledCommunityPlugins = filterAttrs (_: p: p.enable) vaultCfg.community-plugins;
-        communityPluginsList = attrNames enabledCommunityPlugins;
-        
-        vaultPath = if hasPrefix "~/" vaultCfg.path then
-          "${config.home.homeDirectory}/${removePrefix "~/" vaultCfg.path}"
-        else
-          vaultCfg.path;
-          
-        obsidianDir = "${vaultPath}/.obsidian";
-        
-        corePluginsJson = builtins.toJSON corePluginsList;
-        communityPluginsJson = builtins.toJSON communityPluginsList;
-        
-        corePluginConfigs = mapAttrs (_: p: p.config) enabledCorePlugins;
-        appJson = corePluginConfigs // (vaultCfg.extraConfig.app or {});
-        
-        coreFile = if corePluginsList != [] then { "${obsidianDir}/core-plugins.json".text = corePluginsJson; } else {};
-        communityFile = if communityPluginsList != [] then { "${obsidianDir}/community-plugins.json".text = communityPluginsJson; } else {};
-        appFile = if appJson != {} then { "${obsidianDir}/app.json".text = builtins.toJSON appJson; } else {};
-        
-        communityDataFiles = foldl' (acc: pn: let
-          config = enabledCommunityPlugins.${pn}.config;
-        in
-          if config != {} then
-            acc // { "${obsidianDir}/plugins/${pn}/data.json".text = builtins.toJSON config; }
+      vaultFiles = mapAttrsToList (vaultName: vaultCfg:
+        let
+          enabled = vaultCfg.enable;
+          enabledCorePlugins =
+            filterAttrs (_: p: p.enable) vaultCfg.core-plugins;
+          corePluginsList = attrNames enabledCorePlugins;
+          enabledCommunityPlugins =
+            filterAttrs (_: p: p.enable) vaultCfg.community-plugins;
+          communityPluginsList = attrNames enabledCommunityPlugins;
+
+          vaultPath = if hasPrefix "~/" vaultCfg.path then
+            "${config.home.homeDirectory}/${removePrefix "~/" vaultCfg.path}"
           else
-            acc
-        ) {} communityPluginsList;
-        
-        extraConfigFiltered = removeAttrs vaultCfg.extraConfig [ "app" ];
-        extraConfigFiles = mapAttrs' (fn: content: nameValuePair
-          "${obsidianDir}/${fn}.json" { text = builtins.toJSON content; }
-        ) (filterAttrs (_: content: content != {}) extraConfigFiltered);
-        
-        allFiles = coreFile // communityFile // appFile // communityDataFiles // extraConfigFiles;
-      in
-        mkIf enabled allFiles
-      ) cfg.vaults;
-    in
-      foldl' (a: b: a // b) {} vaultFiles;
+            vaultCfg.path;
+
+          obsidianDir = "${vaultPath}/.obsidian";
+
+          corePluginsJson = builtins.toJSON corePluginsList;
+          communityPluginsJson = builtins.toJSON communityPluginsList;
+
+          corePluginConfigs = mapAttrs (_: p: p.config) enabledCorePlugins;
+          appJson = corePluginConfigs // (vaultCfg.extraConfig.app or { });
+
+          coreFile = if corePluginsList != [ ] then {
+            "${obsidianDir}/core-plugins.json".text = corePluginsJson;
+          } else
+            { };
+          communityFile = if communityPluginsList != [ ] then {
+            "${obsidianDir}/community-plugins.json".text = communityPluginsJson;
+          } else
+            { };
+          appFile = if appJson != { } then {
+            "${obsidianDir}/app.json".text = builtins.toJSON appJson;
+          } else
+            { };
+
+          communityDataFiles = foldl' (acc: pn:
+            let config = enabledCommunityPlugins.${pn}.config;
+            in if config != { } then
+              acc // {
+                "${obsidianDir}/plugins/${pn}/data.json".text =
+                  builtins.toJSON config;
+              }
+            else
+              acc) { } communityPluginsList;
+
+          extraConfigFiltered = removeAttrs vaultCfg.extraConfig [ "app" ];
+          extraConfigFiles = mapAttrs' (fn: content:
+            nameValuePair "${obsidianDir}/${fn}.json" {
+              text = builtins.toJSON content;
+            }) (filterAttrs (_: content: content != { }) extraConfigFiltered);
+
+          allFiles = coreFile // communityFile // appFile // communityDataFiles
+            // extraConfigFiles;
+        in mkIf enabled allFiles) cfg.vaults;
+    in foldl' (a: b: a // b) { } vaultFiles;
   };
 }
